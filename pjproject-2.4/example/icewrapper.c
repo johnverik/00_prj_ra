a8600c04156c607eec399fda406ffc7c75d11981
john le <huy.le@veriksystems.com>
2015-06-17 17:53:50 +0700

    build OK

--- pjproject-2.4/example/icewrapper.c
+++ pjproject-2.4/example/icewrapper.c
@@ -18,78 +18,16 @@
 #include <string.h>
 #include <getopt.h>
 
-
-
-#define THIS_FILE   "verikdemo.c"
-
-/* For this demo app, configure longer STUN keep-alive time
- * so that it does't clutter the screen output.
- */
-#define KA_INTERVAL 300
-
-
-char usrid[256];
-char host_name[256];
-int portno;
-char sdp[1024];
-
-#define MAX_ICE_TRANS 10
+#include "icewrapper.h"
 
 
 
-/* This is our global variables */
-static struct app_t
-{
-    /* Command line options are stored here */
-    struct options
-    {
-        unsigned    comp_cnt;
-        pj_str_t    ns;
-        int	    max_host;
-        pj_bool_t   regular;
-        pj_str_t    stun_srv;
-        pj_str_t    turn_srv;
-        pj_bool_t   turn_tcp;
-        pj_str_t    turn_username;
-        pj_str_t    turn_password;
-        pj_bool_t   turn_fingerprint;
-        const char *log_file;
-    } opt;
+#include "httpwrapper.h"
+#include "xml2wrapper.h"
+
+struct app_t icedemo;
 
 
-    int ice_trans_num;
-    struct ice_trans_s{
-
-        char name[256];
-        /* Our global variables */
-        pj_caching_pool	 cp;
-        pj_pool_t		*pool;
-        pj_thread_t		*thread;
-        pj_bool_t		 thread_quit_flag;
-
-        pj_ice_strans_cfg	 ice_cfg;
-        pj_ice_strans	*icest;
-
-        // TODO: should be an array so that it can support each ICE trans
-        struct rem_info
-        {
-            char		 ufrag[80];
-            char		 pwd[80];
-            unsigned	 comp_cnt;
-            pj_sockaddr	 def_addr[PJ_ICE_MAX_COMP];
-            unsigned	 cand_cnt;
-            pj_ice_sess_cand cand[PJ_ICE_ST_MAX_CAND];
-        } rem;
-
-        FILE		*log_fhnd;
-
-    }ice_trans_list[MAX_ICE_TRANS];
-
-
-    /* Variables to store parsed remote ICE info */
-
-
-} icedemo;
 
 /* Utility to display error messages */
 static void icedemo_perror(const char *title, pj_status_t status)
@@ -1395,6 +1333,149 @@ static struct ice_trans_s* get_ice_tran_from_name(char *name)
 /*
  * Main console loop.
  */
+
+char gUrl[] = "http://115.77.49.188:5001";
+
+enum COMMAND_IDX {
+    CMD_HOME_GET = 0,
+    CMD_DEVICE_GET,
+    CMD_DEVICE_REGISTER,
+    CMD_EXIT,
+    CMD_MAX
+};
+
+
+typedef struct cmd_handler_s{
+    enum COMMAND_IDX cmd_idx;
+    char help[256];
+    int (*cmd_func)(void *arg);
+
+}cmd_handler_t;
+
+
+static int api_device_register(void *arg)
+{
+
+    char register_device[] = "<?xml version=\"1.0\"?> \
+    <deviceRegister> \
+    <device> \
+    <deviceId/> \
+    <uniqueId>Mydevice1</uniqueId> \
+    <modelCode>Sensor</modelCode> \
+    <home> \
+    <description>Test Home</description> \
+    <networkID>networkID1</networkID> \
+    </home> \
+    <firmwareVersion>firmware.01.pvt</firmwareVersion> \
+    </device> \
+    <reRegister>0</reRegister> \
+    <smartDevice> \
+    <description>smart phone</description> \
+    <uniqueId>unq_2305130636</uniqueId> \
+    </smartDevice> \
+            </deviceRegister>";
+
+    char full_url[256];
+    char *buff;
+
+    //printf("[DEBUG] %s, %d  \n", __FUNCTION__, __LINE__ );
+
+    strcpy(full_url, gUrl); // plus URL
+    strcpy(&full_url[strlen(full_url)], "/device/registerDevice"); // plus API
+    http_post_request(full_url, register_device);
+    //printf("[DEBUG] API: %s \n", full_url);
+
+}
+
+
+
+static int api_home_get(void* arg)
+{
+    char full_url[256];
+    char *buff;
+
+    //printf("[DEBUG] %s, %d  \n", __FUNCTION__, __LINE__ );
+
+    strcpy(full_url, gUrl); // plus URL
+    strcpy(&full_url[strlen(full_url)], "/device/getDevicesFromNetwork/"); // plus API
+    sprintf(&full_url[strlen(full_url)], "%s", arg); // plus agrument
+    //printf("[DEBUG] API: %s \n", full_url);
+    http_get_request(full_url, &buff);
+
+    xmlNode *device = xml_get_node_by_name(buff, "DeviceList");
+    xmlNode *cur_node;
+    for (cur_node = device->children; cur_node; cur_node = cur_node->next) {
+        if (cur_node->type == XML_ELEMENT_NODE)
+        {
+            char *device_name = (char *)xmlNodeGetContent(cur_node);
+            printf("\t %s \n", device_name);
+            free(device_name);
+        }
+    }
+
+
+    free(buff);
+    return 0;
+}
+
+static int api_device_get(void* arg)
+{
+    char full_url[256];
+    char *buff;
+
+    strcpy(full_url, gUrl);
+    strcpy(&full_url[strlen(full_url)], "/device/getDevice/");
+    sprintf(&full_url[strlen(full_url)], "%s", arg);
+    //printf("[DEBUG] API: %s \n", full_url);
+
+    http_get_request(full_url, &buff);
+    //printf("DEBUG recieved buffer: \n %s \n", buff);
+    // TODO: fine-tuning the result by using libxml
+    char *value = xml_get_content_by_name(buff, "uniqueId");
+    printf("Device information: \n");
+    printf("\t Device ID: %s \n", value);
+    printf("=============================\n");
+    free(value);
+    free(buff);
+    return 0;
+
+}
+
+int cmd_more(void* arg);
+
+cmd_handler_t cmd_list[CMD_MAX] = {
+    {.cmd_idx = CMD_HOME_GET, .help = "Get all devices in a homenetwork", .cmd_func = api_home_get},
+    {.cmd_idx = CMD_DEVICE_GET, .help = "Get full information of a registered device", .cmd_func = api_device_get },
+    {.cmd_idx = CMD_DEVICE_REGISTER, .help = "Register a device to cloud", .cmd_func = api_device_register },
+    {.cmd_idx = CMD_EXIT, .help = "Exit program", .cmd_func = NULL}
+};
+
+void cmd_print_help()
+{
+    int i = 0;
+    for (i = 0; i < CMD_MAX; i++)
+        printf("%d: \t %s \n", cmd_list[i].cmd_idx, cmd_list[i].help);
+}
+
+
+
+int is_valid_int(const char *str)
+{
+    //
+    if (!*str)
+        return 0;
+    while (*str)
+    {
+        if (!isdigit(*str))
+            return 0;
+        else
+            ++str;
+    }
+
+    return 1;
+}
+
+
 static void icedemo_console(void)
 {
     pj_bool_t app_quit = PJ_FALSE;
@@ -1413,6 +1494,42 @@ static void icedemo_console(void)
         icedemo_show_ice(icetrans);
 
     }
+
+#if 1
+    char cmd[256];
+    memset(cmd, 0, 256);
+    while (printf(">>>") && gets(&cmd[0]) != NULL)
+    {
+        //printf("cmd: %s \n", cmd);
+        if (is_valid_int(cmd))
+        {
+            int idx = atoi(cmd);
+        //printf("[DEBUG] command index : %d \n", idx );
+            switch (idx)
+            {
+            case CMD_HOME_GET:
+                cmd_list[idx].cmd_func("networkID1");
+                break;
+            case CMD_DEVICE_GET:
+                cmd_list[idx].cmd_func("device1");
+                break;
+           case CMD_DEVICE_REGISTER:
+                cmd_list[idx].cmd_func("registerDevice");
+                break;
+            case CMD_EXIT:
+                printf("BYE BYE :-*, :-*\n");
+                exit(0);
+            default:
+                cmd_print_help();
+                break;
+            }
+        }else
+            cmd_print_help();
+
+        memset(cmd, 0, 256);
+    }
+#else
+
 
     peer_put_dsp(usrid, sdp);
     while (!app_quit) {
@@ -1476,6 +1593,7 @@ static void icedemo_console(void)
         }
 
     }
+#endif
 
 }
 
@@ -1517,11 +1635,15 @@ static void icedemo_usage()
 }
 
 
+
+
+
+
+
+
 /*
  * And here's the main()
  */
-
-#ifdef BUILD_VERIKDEMO
 
 
 int main(int argc, char *argv[])
@@ -1633,5 +1755,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-#endif 
